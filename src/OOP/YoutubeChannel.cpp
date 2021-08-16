b9c2fb43cd5941a88ba37517c9b38083f07ce83b
Yuri Melo <yuri.melo@inventcloud.com.br>
2021-08-15 22:54:06 -0300

    Add new class

--- src/OOP/YoutubeChannel.cpp
+++ src/OOP/YoutubeChannel.cpp
@@ -3,33 +3,63 @@
 
 using namespace std;
 
-
 class YouTubeChannel {
-    public:
-        string Name;
-        string OwnerName;
-        int SubscribersCount;
-        list<string> PublishedVideos;
+private:
+    string Name;
+    string OwnerName;
+    int SubscribersCount;
+    list<string> PublishedVideos;
+
+public:
+    YouTubeChannel(string name, string ownerName) {
+        Name = name;
+        OwnerName = ownerName;
+        SubscribersCount = 0;
+    }
+
+    void GetInfo() {
+        cout << "Name: " << Name << endl;
+        cout << "OnwerName: " << OwnerName << endl;
+        cout << "Subscribers Count: " << SubscribersCount << endl;
+        cout << "Video title: " << endl;
+
+        for (string videoTitle : PublishedVideos)
+        {
+            cout << videoTitle << endl;
+        }
+        cout << endl;
+    }
+
+    void InsertVideo(string value) {
+        PublishedVideos.push_back(value);
+    }
+
+    void SeedListOfVideos(int amount) {
+        for (int i = 0; i < amount; i++)
+        {
+            PublishedVideos.push_back("Something...");
+        }
+    }
+
+    void Subscribe() {
+        ++SubscribersCount;
+    }
+
+    void Unsubscribe() {
+        if (SubscribersCount > 0)
+            --SubscribersCount;
+    }
+};
+
+class CookingYoutubeChannel : public YouTubeChannel {
+public:
+    CookingYoutubeChannel(string name, string ownerName): YouTubeChannel(name, ownerName) {
+    }
 };
 
 int main() {
-    
-    YouTubeChannel ytChannel;
-    ytChannel.Name = "Yuri Melo";
-    ytChannel.OwnerName = "Yuri Melo";
-    ytChannel.SubscribersCount = 15;
-    ytChannel.PublishedVideos = { 
-        "01. Gettings Started with C++ Part I",
-        "02. Gettings Started with C++ Part II",
-        "03. Gettings Started with C++ Part III"
-    };
+    CookingYoutubeChannel amyChannel("Amy's kitchen", "Amy");
+    amyChannel.GetInfo();
 
-    cout << "Name: " << ytChannel.Name << endl;
-    cout << "OnwerName: " << ytChannel.OwnerName << endl;
-    cout << "Subscribers Count: " << ytChannel.SubscribersCount << endl;
-    cout << "Video title: " << endl;
-
-    for(string videoTitle: ytChannel.PublishedVideos) {
-        cout << videoTitle << endl;
-    }
+    return 0;
 }
