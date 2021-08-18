c1a6c993ed751ee00dc24c8fd401de096982348d
Yuri Melo <yuri.melo@inventcloud.com.br>
2021-08-17 23:38:31 -0300

    new codes

--- src/OOP/YoutubeChannel.cpp
+++ src/OOP/YoutubeChannel.cpp
@@ -11,12 +11,14 @@ private:
 
 protected:
     string OwnerName;
+    int ContentQuality;
 
 public:
     YouTubeChannel(string name, string ownerName) {
         Name = name;
         OwnerName = ownerName;
         SubscribersCount = 0;
+        ContentQuality = 0;
     }
 
     void GetInfo() {
@@ -51,6 +53,15 @@ public:
         if (SubscribersCount > 0)
             --SubscribersCount;
     }
+
+    void CheckAnalytics() {
+        if(ContentQuality < 5) {
+            cout << Name << " has a bad quality content" << endl;
+        }
+        else {
+            cout << Name << " has a great quality content" << endl;
+        }
+    }
 };
 
 class CookingYoutubeChannel : public YouTubeChannel {
@@ -60,17 +71,43 @@ public:
 
     void Pratice() {
         cout << OwnerName << " is practicing cooking, learning new recipesm, experimenting with spices..." << endl;
+        ++ContentQuality;
+    }
+};
+
+class SingersYoutubeChannel : public YouTubeChannel {
+public:
+    SingersYoutubeChannel(string name, string ownerName): YouTubeChannel(name, ownerName) {
+    }
+
+    void Practice() {
+        cout << OwnerName << " is taking singing classes, learning new songs, learning now to dance..." << endl;
+        ++ContentQuality;
     }
 };
 
 int main() {
     CookingYoutubeChannel amyChannel("Amy's kitchen", "Amy");
-    amyChannel.InsertVideo("Apple pie");
-    amyChannel.InsertVideo("Chocolate cake");
-    amyChannel.Subscribe();
-    amyChannel.Subscribe();
-    amyChannel.GetInfo();
+    SingersYoutubeChannel charliePuthChannel("Charlie Puth", "Charlie Puth");
+
     amyChannel.Pratice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+    charliePuthChannel.Practice();
+
+    YouTubeChannel* yt1 = &amyChannel;
+    YouTubeChannel* yt2 = &charliePuthChannel; 
+
+    yt1->CheckAnalytics();
+    yt2->CheckAnalytics();
 
     return 0;
 }
