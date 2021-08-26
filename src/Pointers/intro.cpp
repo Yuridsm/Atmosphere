0207e6079c8a06729ae1e81a3d0f1a5870908c19
Yuri Melo <yurifullstack@gmail.com>
2021-08-26 12:03:00 -0300

    Introduction to Pointer in C+

---
+++ src/Pointers/intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 90;
+    int * ptr = & n;
+
+    cout << "Value that stored in n variable: " << n << endl;
+    cout << "Location address of n variable in physic memory: " << &n << endl;
+    cout << "Value that stored in prt pointer: " << ptr << endl;
+    cout << "Value pointed by ptr pointer: " << *ptr << endl;
+
+    if(&n == ptr) {
+        cout << "The same address of n variable is ptr value" << endl;
+    }
+}
