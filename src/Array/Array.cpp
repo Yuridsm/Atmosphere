abd2917491111be1bef8df4e148f4dd80e1e6932
Yuri Melo <yurifullstack@gmail.com>
2021-09-29 22:21:56 -0300

    Derreferencing array

--- src/Array/Array.cpp
+++ src/Array/Array.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+// Pointers to arrays
+ 
 int main() {
-    
+    int luckyNumbers[5] = { 1, 2, 3, 4, 5 }; 
+    cout << luckyNumbers << endl;
+    cout << &luckyNumbers[0] << endl;
+    cout << luckyNumbers[2] << endl;
+    cout << *(luckyNumbers+2) << endl;
 
     return 0;
 }
