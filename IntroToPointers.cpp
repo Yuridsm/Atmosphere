53c3c078dd14acc22f338849ae148557c561dfd1
Yuri Melo <yurifullstack@gmail.com>
2023-05-09 00:17:22 -0300

    Look at about pointers

---
+++ IntroToPointers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "IntroToPointers.h"
+
+using namespace std;
+
+void ExecuteUseCase()
+{
+	cout << "Hello Pointers" << endl;
+}
+
+void f(int* pi) 
+{
+	void* pv = pi; // Implicit conversion of int* to void*
+
+	int* pi2 = static_cast<int*>(pv);
+	cout << * pi2 << endl;
+}
