8e36436db67debc2f60a24dcfbdd193e4a6868a7
Yuri Melo <yurifullstack@gmail.com>
2023-11-18 22:47:43 -0300

    Create a XML Parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,72 +2,17 @@
 
 #include "Headers/Stack.h"
 #include "UseCase/SignUpUseCase.h"
+#include "xmlParser/lxml.hpp"
 
 using namespace std;
 
-class IOrder
-{
-public:
-	virtual void Execute() = 0;
-};
-
-class OrderA : public IOrder
-{
-public:
-	void Execute() override
-	{
-		std::cout << "Executing A\n";
-	}
-};
-
-class OrderB : public IOrder
-{
-public:
-	void Execute() override
-	{
-		std::cout << "Executing B\n";
-	}
-};
-
-class OrderC : public IOrder
-{
-public:
-	void Execute() override
-	{
-		std::cout << "Executing C\n";
-	}
-};
-
-class OrderUseCase
-{
-private:
-	IOrder& _order;
-
-public:
-	OrderUseCase(IOrder& order) : _order(order) 
-	{
-
-	}
-
-	void Execute()
-	{
-		_order.Execute();
-	}
-};
-
 int main()
 {
-	std::cout << "I'm running..." << std::endl;
-	Stack stack;
-
-	stack.Push('A');
-	stack.Push('H');
-
-	while (!stack.Empty())
+	XMLDocument doc;
+	if (XMLDocument_load(&doc, ".\\src\\xmlParser\\test.xml"))
 	{
-		Item i;
-		stack.Pop(i);
-		cout << i << endl;
+		printf("%s\n", doc.source);
+		XMLDocument_free(&doc);
 	}
 
 	return 0;
